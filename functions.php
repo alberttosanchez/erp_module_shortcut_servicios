cd213e38bc1590b1c47d5924b54e64977c19a3cb
Alberto Sánchez Germán <alberto.sanchez@juventud.gob.do>
2025-04-09 14:23:54 -0400

    init commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php // Aqui van las funciones del modulo funciones del modulo
+
+/*
+Module Name     : Servicios
+Version         : 1.0.0  
+Description     : Modulo que redirige al portal de domain.local
+Author          : Alberto E. Sánchez G.
+Web             : http://domain.local
+isLink          : 1
+Licence         : GNU GPL 3.0
+*/
